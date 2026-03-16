f62c97b7d21dd9a58263df6e13cbbbfdc80f1645
hokenz123 <dima.2016.carawanov@gmail.com>
2026-03-17 00:01:42 +0300

    Added functionality

--- frag.hpp
+++ frag.hpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 
+#include <functional>
+#include <fstream>
 #include <queue>
 #include <cstring>
 #include <array>
@@ -9,10 +11,10 @@
 #include <cstdint>
 
 namespace AProtocol{
-    static const int BlockSize = 512; // bytes
-    static const int HeaderSize = 3;
+    static const int BlockSize = 1500; // bytes
+    static const int HeaderSize = 4;
     static const int BodySize = BlockSize - HeaderSize;
-    static const int PacketBufferSize = 4096;
+    // static const int PacketBufferSize = 4096;
     static_assert(BlockSize > HeaderSize, "Block must be bigger then header");
 
     class Block {
@@ -21,36 +23,28 @@ namespace AProtocol{
         int is_last                  : 1;
         int padding                  : 23;
 
-        std::array<char, BodySize> body;
+        char body[BodySize];
     public:
-        Block(unsigned int count, std::array<char, BodySize> data) : 
+        Block(unsigned int count, const char data[BodySize]) : 
             block_count(count),
             is_last(false),
-            next_packet_offset(0),
-            padding(0),
-            body(data)
-        {};
-        Block(unsigned int count, bool is_last, std::array<char, BodySize> data) : 
+            padding(0)
+        {
+            memcpy(body, data, BodySize);
+        };
+        Block(unsigned int count, bool is_last, const char data[BodySize]) : 
             block_count(count),
             is_last(is_last),
-            next_packet_offset(0),
-            padding(0),
-            body(data)
-        {};
-        Block(unsigned int count, unsigned int ofsset, std::array<char, BodySize> data) : 
-            block_count(count),
-            is_last(false),
-            next_packet_offset(ofsset),
-            padding(0),
-            body(data)
-        {};
+            padding(0)
+        {
+            memcpy(body, data, BodySize);
+        };
 
         bool isLast() const { return is_last; }
-        std::array<char, BodySize>& getBody() { return body; }
+        const char* getBody() const { return body; }
         unsigned int getCount() const { return block_count; }
-        unsigned int getOffset() const { return next_packet_offset; }
-        void toggleLast() { is_last = !is_last; }
-        void setOffset (unsigned int offset) { next_packet_offset = offset; }
+        // void toggleLast() { is_last = !is_last; }
+        void setLast(bool value) { is_last = value; }
     };
 
     class Packet {
@@ -64,59 +58,32 @@ namespace AProtocol{
         const std::vector<char> getBody() const { return body; }
     };
 
-    void fragment (std::queue<Block>& Block_Queue, std::queue<Packet>& Packet_Queue) {
-        if (Packet_Queue.size() == 0) return;
-        for (;!Packet_Queue.empty();){
-            Packet current_packet = Packet_Queue.front();
-            char buff[BodySize] = { 0 };
-            unsigned int cursor = 0;
-            size_t last_count = 0;
-            if (Block_Queue.size() != 0)
-                last_count = Block_Queue.back().getCount();
-            if (Block_Queue.size() != 0 && Block_Queue.back().getOffset() != 0) {
-                assert(cursor <= current_packet.size() && "Cursor out of bounds");
-                const unsigned int offset = Block_Queue.back().getOffset();
-                const unsigned int read_size = BodySize - offset;
-                memcpy(buff, &current_packet.getBody().data()[cursor], read_size);
-                std::array<char, BodySize> tmp;
-                std::copy(buff, buff + read_size, std::begin(Block_Queue.back().getBody()) + offset);
-                // std::cout << "Changing block: ";
-                // print_array<char, BodySize>(tmp);
-                cursor += read_size;
-            }
-            if (current_packet.size() - cursor >= BodySize){
-                for (unsigned int i = 0; i < (current_packet.size() - cursor) / BodySize; i++){
-                    assert(cursor <= current_packet.size() && "Cursor out of bounds");
-                    memcpy(buff, &current_packet.getBody().data()[cursor], BodySize);
-                    std::array<char, BodySize> tmp;
-                    memset(tmp.data(), 0, BodySize);
-                    std::copy(buff, buff + BodySize, std::begin(tmp));
-                    Block b(last_count, tmp);
-                    Block_Queue.push(b);
-                    // std::cout << "Slashing packet (" << (int)tmp[1] << "): ";
-                    // print_array<char, BodySize>(tmp);
-                    cursor += BodySize;
-                    last_count++;
-                }
-            }
-            // Дописать остаток пакета
-            if ((current_packet.size() - cursor) % BodySize != 0 || cursor < current_packet.size()) {
-                assert(cursor <= current_packet.size() && "Cursor out of bounds");
-                assert(current_packet.size() - cursor <= BodySize && "Proper entry to if statement"); // TODO придумать сообщение лучше
-                
-                unsigned int offset = current_packet.size() - cursor;
-                memcpy(buff, &current_packet.getBody().data()[cursor], offset);
-                std::array<char, BodySize> tmp;
-                std::fill(std::begin(tmp), std::end(tmp), 0);
-                std::copy(buff, buff + offset, std::begin(tmp));
-                Block_Queue.push(Block(last_count+1, offset, tmp));
-                // std::cout << "Adding to block: ";
-                // print_array<char, BodySize>(tmp);
-            }
-            Packet_Queue.pop();
+    void print_array(std::array<char, BodySize> arr) {
+        for (const auto& e : arr)
+            std::cout << e;
+        std::cout << std::endl;
+    }
 
-            // TODO wait and flag
-        };
-        Block_Queue.back().toggleLast();
+    void fragment (std::ifstream& fileStream, std::function<void(Block)> func) {
+        char body[BodySize];
+        unsigned int block_count = 0;
+        while (fileStream)
+        {
+            fileStream.read(body, BodySize);
+            std::streamsize bytesRead = fileStream.gcount();
+            if (bytesRead == 0){
+                // EOF
+                break;
+            }
+            bool is_last = false;
+            if (bytesRead < BodySize){
+                memset(body+bytesRead, 0, BodySize-bytesRead);
+                is_last = true;
+            }
+            Block b(block_count, is_last, body);
+            // blockQueue.push(b);
+            func(b);
+            block_count++;
+        }
     };
 }
